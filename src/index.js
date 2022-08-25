331ae2ba2ce74b254d40f045893e4bba6dead0db
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-08-25 18:02:42 +0300

    completed tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ form = document.querySelector('form')
+ form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    myToDoList(e.target.newTask.value)
+    form.reset()
+  })
+})
+
+function myToDoList (todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteBtn)
+  btn.textContent = 'x'
+  li.textContent = `${todo}  `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function deleteBtn (e) {
+  e.target.parentNode.remove()
+}
